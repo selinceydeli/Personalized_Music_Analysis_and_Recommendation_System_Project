f73681d2bbeedd70082d267d252062cf77e3f05d
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2023-10-29 01:46:22 +0300

    tailor_music database is opened and tables are created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('date_of_birth');
+            $table->string('language');
+            $table->string('subscription');
+            $table->string('rate_limit');
             $table->rememberToken();
             $table->timestamps();
         });
