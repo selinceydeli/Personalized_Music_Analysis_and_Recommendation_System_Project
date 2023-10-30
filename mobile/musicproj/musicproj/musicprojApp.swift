afb25bebc7f220bda48ae9f9118219817ca6457b
simalyucel <simal@sabanciuniv.edu>
2023-10-30 14:51:36 +0300

    xcode project added

---
+++ mobile/musicproj/musicproj/musicprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicprojApp.swift
+//  musicproj
+//
+//  Created by Şimal on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct musicprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobile/musicproj/musicprojUITests/musicprojUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  musicprojUITestsLaunchTests.swift
+//  musicprojUITests
+//
+//  Created by Şimal on 30.10.2023.
+//
+
+import XCTest
+
+class musicprojUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
