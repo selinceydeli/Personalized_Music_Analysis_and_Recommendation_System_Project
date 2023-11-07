e54738afa52852da57d1348ec37eee870eb7d594
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2023-11-07 12:43:01 +0300

    PMARS-74 album controller revised after testing

--- app/Http/Controllers/AlbumRatingController.php
+++ app/Http/Controllers/AlbumRatingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AlbumRating;
 use App\Http\Resources\AlbumRatingResource;
+use Illuminate\Support\Facades\DB;
 
 class AlbumRatingController extends Controller
 {
@@ -71,5 +72,55 @@ class AlbumRatingController extends Controller
                 "message" => "Album rating not found"
             ], 404);
         }
+    }
+    
+    // Methods defined for analysis functionality
+    public function topRatedAlbumsByEra($username, $era)
+    {
+        // Define the start and end years based on the era
+        switch ($era) {
+            case '50s':
+                $yearsRange = ['1950-01-01', '1959-12-31'];
+                break;
+            case '60s':
+                $yearsRange = ['1960-01-01', '1969-12-31'];
+                break;
+            case '70s':
+                $yearsRange = ['1970-01-01', '1979-12-31'];
+                break;
+            case '80s':
+                $yearsRange = ['1980-01-01', '1989-12-31'];
+                break;
+            case '90s':
+                $yearsRange = ['1990-01-01', '1999-12-31'];
+                break;
+            case '20s':
+                $yearsRange = ['2000-01-01', '2029-12-31'];
+                break;
+            default:
+                // Handle invalid era input or set a default range
+                return response()->json(['error' => 'Invalid era provided.'], 400);
+        }
+
+        // Create a subquery for the average rating of albums by the user
+        $ratingSubQuery = DB::table('album_ratings')
+            ->select('album_id', DB::raw('AVG(rating) as average_rating'))
+            ->where('username', $username)
+            ->groupBy('album_id');
+
+        // Join the tables and select the top 10 albums from the specified era
+        $topAlbums = DB::table('albums')
+            ->joinSub($ratingSubQuery, 'rating', function ($join) {
+                $join->on('albums.id', '=', 'rating.album_id');
+            })
+            ->join('songs', 'albums.id', '=', 'songs.album_id')
+            ->select('albums.name', 'albums.image_url', 'rating.average_rating')
+            ->whereBetween('songs.publ_date', $yearsRange)
+            ->groupBy('albums.id', 'albums.name', 'albums.image_url')
+            ->orderBy('average_rating', 'DESC')
+            ->take(10)
+            ->get();
+
+        return response()->json($topAlbums);
     }
 }
