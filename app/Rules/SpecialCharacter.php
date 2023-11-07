d64d27d511ec0835de89f82c46ba515187e147cd
CanberkTahil <canberktahil@sabanciuniv.edu>
2023-11-07 18:39:15 +0300

    Update SpecialCharacter.php

--- app/Rules/SpecialCharacter.php
+++ app/Rules/SpecialCharacter.php
@@ -18,6 +18,7 @@ class SpecialCharacter implements ValidationRule
     if (!preg_match('/[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/', $value)) {
         // If no special character is found, report an error using $fail
         $fail("The $attribute must contain at least one special character.");
+        hjhjjh
     }
 }
 
