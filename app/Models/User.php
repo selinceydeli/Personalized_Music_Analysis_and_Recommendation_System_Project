5e3647c42451eeb1df791a2570f7cd693af92fac
CanberkTahil <canberktahil@sabanciuniv.edu>
2023-11-22 16:05:58 +0300

    https://musictailor.atlassian.net/browse/PMARS-99

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'username' => 'string', // Cast username as a string
         'password' => 'hashed',
     ];
 }
